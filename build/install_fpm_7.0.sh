b6600541c004fd68394f6832246472d855a83889
Landrok <Landrok@users.noreply.github.com>
2016-10-26 01:38:45 +0200

    Disable mpm_prefork

--- build/install_fpm_7.0.sh
+++ build/install_fpm_7.0.sh
@@ -8,6 +8,7 @@ sudo apt-get install apache2 libapache2-mod-fastcgi apache2-mpm-worker php7.0-fp
 
 # enable php-fpm
 sudo cp ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf.default ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf
+sudo a2dismod mpm_prefork
 sudo a2enmod rewrite actions fastcgi alias mpm_worker
 echo "cgi.fix_pathinfo = 1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 
