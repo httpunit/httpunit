d3d4d872135e091994eaeeae632835f806ef7630
Landrok <Landrok@users.noreply.github.com>
2016-10-26 01:26:20 +0200

    Fix fastcgi config

--- build/install_fpm_7.0.sh
+++ build/install_fpm_7.0.sh
@@ -8,12 +8,12 @@ sudo apt-get install apache2 libapache2-mod-fastcgi apache2-mpm-worker php7.0-fp
 
 # enable php-fpm
 sudo cp ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf.default ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf
-sudo a2enmod rewrite actions fastcgi alias
+sudo a2enmod rewrite actions fastcgi alias mpm_worker
 echo "cgi.fix_pathinfo = 1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 
 # configure apache virtual hosts
 sudo cp -f build/php7/travis-ci-apache /etc/apache2/sites-available/default
 sudo sed -e "s?%TRAVIS_BUILD_DIR%?$(pwd)?g" --in-place /etc/apache2/sites-available/default
 
-sudo service apache2 restart
 sudo service php7.0-fpm restart
+sudo service apache2 restart
