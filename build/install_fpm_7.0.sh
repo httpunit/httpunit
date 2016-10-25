c82af0bfda3d116ec649bccfcd8cf19f3f9b1763
Landrok <Landrok@users.noreply.github.com>
2016-10-26 00:10:49 +0200

    Add support for PHP7

---
+++ build/install_fpm_7.0.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install apache2 libapache2-mod-fastcgi php7.0-fpm
+
+# enable php-fpm
+sudo cp ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf.default ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf
+sudo a2enmod rewrite actions fastcgi alias
+echo "cgi.fix_pathinfo = 1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+
+# configure apache virtual hosts
+sudo cp -f build/php5/travis-ci-apache /etc/apache2/sites-available/default
+sudo sed -e "s?%TRAVIS_BUILD_DIR%?$(pwd)?g" --in-place /etc/apache2/sites-available/default
+
+sudo service apache2 restart
+sudo service php7.0-fpm restart
