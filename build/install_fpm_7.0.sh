0e7d41b884512c0c71049f829136b2287a70aa32
Landrok <Landrok@users.noreply.github.com>
2016-10-26 02:31:50 +0200

    Add listen directive

--- build/install_fpm_7.0.sh
+++ build/install_fpm_7.0.sh
@@ -8,7 +8,8 @@ sudo apt-get install apache2 libapache2-mod-fastcgi php7.0-fpm php7.0-curl
 
 # enable php-fpm
 sudo cp ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf.default ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf
-sudo a2dismod mpm_prefork
+echo "listen = 127.0.0.1:9000" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf
+echo "listen.allowed_clients = 127.0.0.1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf
 sudo a2enmod rewrite actions fastcgi alias
 echo "cgi.fix_pathinfo = 1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 
@@ -16,7 +17,7 @@ echo "cgi.fix_pathinfo = 1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php
 sudo cp -f build/php7/travis-ci-apache /etc/apache2/sites-available/default
 sudo sed -e "s?%TRAVIS_BUILD_DIR%?$(pwd)?g" --in-place /etc/apache2/sites-available/default
 
-sudo service php7.0-fpm restart
 sudo service apache2 restart
+sudo service php7.0-fpm restart
 
 cat ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf.default
