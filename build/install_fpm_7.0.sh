7faaecb94a9f863924bf7f651b4ba51b9817505a
Landrok <Landrok@users.noreply.github.com>
2016-10-26 19:27:39 +0200

    tests a workaround php7 and fpm

--- build/install_fpm_7.0.sh
+++ build/install_fpm_7.0.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 
-sudo apt-get install software-properties-common
-sudo add-apt-repository ppa:ondrej/php -y
-
 sudo apt-get update
-sudo apt-get install apache2 libapache2-mod-fastcgi php7.0-fpm php7.0-curl
+sudo apt-get install apache2 libapache2-mod-fastcgi
 
 # enable php-fpm
 sudo cp ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf.default ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf
-sudo echo "listen = 127.0.0.1:9000" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf
-sudo echo "listen.allowed_clients = 127.0.0.1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf
 sudo a2enmod rewrite actions fastcgi alias
+
+# work around travis issue #3385
+if [ "$TRAVIS_PHP_VERSION" = "7.0" -a -n "$(ls -A ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.d)" ]; then
+  sudo cp ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.d/www.conf.default ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.d/www.conf
+fi
+
 echo "cgi.fix_pathinfo = 1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+echo "always_populate_raw_post_data = -1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+
+
 
 # configure apache virtual hosts
 sudo cp -f build/php7/travis-ci-apache /etc/apache2/sites-available/default
 sudo sed -e "s?%TRAVIS_BUILD_DIR%?$(pwd)?g" --in-place /etc/apache2/sites-available/default
+
+
 
 sudo service php7.0-fpm restart
 sudo service apache2 restart
