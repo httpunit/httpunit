ea04077a076cd37ac26ddce8901e2d62d96f187f
Landrok <Landrok@users.noreply.github.com>
2016-10-26 19:58:41 +0200

    Clean build repository

--- build/install_fpm_5.4.sh
+++ build/install_fpm_5.4.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-get install apache2 libapache2-mod-fastcgi php5-fpm
+sudo apt-get install php5-fpm
 
 # enable php-fpm
 sudo cp ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf.default ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf
@@ -8,7 +8,7 @@ sudo a2enmod rewrite actions fastcgi alias
 echo "cgi.fix_pathinfo = 1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 
 # configure apache virtual hosts
-sudo cp -f build/php5/travis-ci-apache /etc/apache2/sites-available/default
+sudo cp -f build/apache_php/travis-ci-apache /etc/apache2/sites-available/default
 sudo sed -e "s?%TRAVIS_BUILD_DIR%?$(pwd)?g" --in-place /etc/apache2/sites-available/default
 
 sudo service apache2 restart
--- build/install_fpm_7.0.sh
+++ build/install_fpm_7.0.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-sudo apt-get install apache2 libapache2-mod-fastcgi
-
 # enable php-fpm
 sudo cp ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf.default ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf
 sudo a2enmod rewrite actions fastcgi alias
@@ -15,7 +13,7 @@ echo "cgi.fix_pathinfo = 1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php
 echo "always_populate_raw_post_data = -1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 
 # configure apache virtual hosts
-sudo cp -f build/php7/travis-ci-apache /etc/apache2/sites-available/default
+sudo cp -f build/apache_php/travis-ci-apache /etc/apache2/sites-available/default
 sudo sed -e "s?%TRAVIS_BUILD_DIR%?$(pwd)?g" --in-place /etc/apache2/sites-available/default
 
 # services
